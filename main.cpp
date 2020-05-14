2fe336a486bdcf0354ae141bce879668239f7d9d
matkatmusic <matkatmusic@gmail.com>
2020-05-13 20:22:49 -0700

    added part 3 instructions

--- main.cpp
+++ main.cpp
@@ -1,66 +1,38 @@
 /*
- Project 4 - Part 2 / 9
- Video: Chapter 3 Part 6
+ Project 4 - Part 3 / 9
+ Video: Chapter 4 Part 3 
+ Casting
+
+ Create a branch named Part3
  
- Create a branch named Part2
+ do not remove anything from main().  you'll be revising your main() to work with these new code changes.
  
- New/This/Pointers/References conclusion
+    Build/Run often with this task to make sure you're not breaking the code with each step.
+    I recommend committing after you get each step working so you can revert to a working version easily if needed.
+
+ 1) remove your functions that accepted a User-Defined Type
  
- Build/Run often with this task to make sure you're not breaking the code with each step.
- I recommend committing after you get each step working so you can revert to a working version easily if needed.
+ 2) remove any getValue() functions if you added them
  
- 0) in the blank space below, declare/define an empty struct named 'A' on a single Line.
- on the lines below it, write a struct named 'HeapA' that correctly shows how to own an instance of 'A'
- on the heap without leaking, without using smart pointers.
+ 3) move all of your add/subtract/multiply/divide implementations out of the class.
+  
+ 4) add user-defined conversion functions that convert to the numeric type your object holds.
+        i.e. if your type holds an int, you'll need an operator int() function.
+ 
+ 5) make your member variable private.
+         this conversion function should be the ONLY WAY to access the held value.
+         use the proper casting technique to invoke this conversion function
+ 
+ 6) clean up any forward declaration that you might have.
+ 
+ 7) make sure it compiles & runs without errors.
+ 
+ 8) use your knowledge of casting to remove any conversion warnings. 
+ 
  */
 
-struct A {};
 
-struct HeapA
-{
-    A* a;
-    HeapA(A* a_) : a(a_) { }
-    ~HeapA() { delete a; }
-};
 
-/*
- 1) Edit your 3 structs so that they own a heap-allocated primitive type without using smart pointers
- IntType should own a heap-allocated int, for example.
- 
- 2) give it a constructor that takes the appropriate primitive
- this argument will initialize the owned primitive's value.
- i.e. if you're owning an int on the heap, your ctor argument will initialize that heap-allocated int's value.
- 
- 3) modify those add/subtract/divide/multiply member functions from chapter 2 on it
- a) make them modify the owned numeric type
- b) set them up so they can be chained together.
- i.e.
- DoubleType dt(3.5);
- dt.add(3.0).multiply(-2.5).divide(7.2); //an example of chaining
- 
- 4) write add/subtract/divide/multiply member functions for each type that take your 3 UDTs
- These are in addition to your member functions that take primitives
- for example, IntType::divide(const DoubleType& dt);
- These functions should return the result of calling the function that takes the primitive.
- 
- 5) print out the results with some creative couts
- i.e.
- FloatType ft(0.1f);
- IntType it(3);
- std::cout << "adding 3 and subtracting 'it' from 'ft' results in the following value: " << *ft.add(2.f).subtract( it ).value << std::endl;  //note the dereference of the `value` member of `ft`
- 
- 6) Don't let your heap-allocated owned type leak!
- 
- 7) click the [run] button.  Clear up any errors or warnings as best you can.
- 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- Make a pull request after you make your first commit and pin the pull request link to our DM thread.
- 
- send me a DM to check your pull request
- 
- Wait for my code review.
- */
 
 
 #include <iostream>
