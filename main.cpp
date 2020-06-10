f189442ff1924b889bbc64dfc7cb3ef0fe4e9f43
D300 <mail@dirkhandreke.com>
2020-06-10 10:48:47 +0000

    console outputs requested output

--- main.cpp
+++ main.cpp
@@ -45,7 +45,10 @@ struct HeapA
 {
     A* a;
     HeapA(A* a_) : a(a_) { }
-    ~HeapA() { delete a; }
+    ~HeapA() 
+    { 
+        a = nullptr; 
+    }
 };
 
 
@@ -140,7 +143,7 @@ FloatType& FloatType::divide( float value_ )
 {
     if (value_ == 0.f)
     {
-        std::cout << "watch out when dividing with zero!\n" << std::endl;
+        std::cout << "error: integer division by zero is an error and will crash the program!" << std::endl;
     }
 
     *ownedFloat /= value_;
@@ -171,7 +174,7 @@ DoubleType& DoubleType::divide( double value )
 {
     if (value == 0.)
     {
-        std::cout << "watch out when dividing with zero!\n" << std::endl;
+        std::cout << "error: integer division by zero is an error and will crash the program!" << std::endl;
     }
     
     *ownedDouble /= value;
@@ -204,7 +207,7 @@ IntType& IntType::divide( int value )
 {
     if(value == 0)
     {
-        std::cout << "\nwatch out when dividing with zero! especially when using int. returned zero\n" << std::endl;
+        std::cout << "error: integer division by zero is an error and will crash the program!" << std::endl;
     }
     else
     {
@@ -214,17 +217,34 @@ IntType& IntType::divide( int value )
 }
 
 //=============================================================
+
+void part3()
+{
+    FloatType ft( 5.5f );
+    DoubleType dt( 11.1 );
+    IntType it ( 34 );
+    DoubleType pi( 3.14 );
+
+    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( static_cast<float>(ft) ).multiply( static_cast<float>(ft) ).divide( static_cast<int>(it) ) << std::endl;
+    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( static_cast<double>(3) ).add( static_cast<int>(it) ) << std::endl;
+    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide( static_cast<int>(pi) ).multiply( static_cast<int>(dt) ).subtract( static_cast<int>(ft) ) << std::endl;
+    std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
+    std::cout << it.multiply( static_cast<int>(it) ).divide(0).divide( static_cast<int>(0.0f) ).divide( static_cast<int>(0.0) ) <<std::endl;
+    
+    std::cout << "FloatType x IntType  =  " << it.multiply( static_cast<int>(ft) )<< std::endl;
+    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( static_cast<int>(dt) ).add( static_cast<int>(ft) ).multiply( 24 ) << std::endl;
+}
+
 //=============================================================
 
 int main()
 {   
     //testing instruction 0
-    A a;
-    HeapA heapA(&a); 
+    HeapA heapA(new A()); 
 
     //assign heap primitives
     FloatType ft ( 2.0f );
-    DoubleType dt ( 2 );
+    DoubleType dt ( 2.0 );
     IntType it ( 2 ) ;
 
     std::cout << "FloatType add result=" << ft.add(static_cast<int>(ft)) << std::endl;
@@ -232,60 +252,49 @@ int main()
     std::cout << "FloatType multiply result=" << ft.multiply(static_cast<int>(ft)) << std::endl;
     std::cout << "FloatType divide result=" << ft.divide(static_cast<float>(static_cast<float>(ft)*static_cast<float>(ft))) << std::endl << std::endl;
 
-    /*
-    std::cout << "DoubleType add result=" << dt.add(2.0).value << std::endl;
-    std::cout << "DoubleType subtract result=" << dt.subtract(2.0).value << std::endl;
-    std::cout << "DoubleType multiply result=" << dt.multiply(2.0).value << std::endl;
-    std::cout << "DoubleType divide result=" << dt.divide(5.f).value << std::endl << std::endl;
+    // 4 2 4 0.8
+    std::cout << "DoubleType add result=" << dt.add(static_cast<double>(dt)) << std::endl;
+    std::cout << "DoubleType subtract result=" << dt.subtract(static_cast<double>(it)) << std::endl;
+    std::cout << "DoubleType multiply result=" << dt.multiply(static_cast<double>(dt)) << std::endl;
+    std::cout << "DoubleType divide result=" << dt.divide(static_cast<double>(5.0f)) << std::endl << std::endl;
 
-    std::cout << "IntType add result=" << it.add(2).value << std::endl;
-    std::cout << "IntType subtract result=" << it.subtract(2).value << std::endl;
-    std::cout << "IntType multiply result=" << it.multiply(2).value << std::endl;
-    std::cout << "IntType divide result=" << it.divide(3).value << std::endl << std::endl;
-    std::cout << "Chain calculation = " << (it.multiply(1000).divide(2).subtract(10).add(100)).value << std::endl;
+    
+    std::cout << "IntType add result=" << ( it.add(2) ) << std::endl;
+    std::cout << "IntType subtract result=" << ( it.subtract(2) ) << std::endl;
+    std::cout << "IntType multiply result=" << ( it.multiply(2) ) << std::endl;
+    std::cout << "IntType divide result=" << ( it.divide(3) ) << std::endl << std::endl;
+    std::cout << "Chain calculation = " << ( it.multiply(1000).divide(2).subtract(10).add(100) ) << std::endl;
 
-        // FloatType object instanciation and method tests
+    //==================
+
+    // FloatType object instanciation and method tests
     // --------
-    std::cout << "New value of ft = (ft + 3.0f) * 1.5f / 5.0f = " << ft.add( 3.0f ).multiply(1.5f).divide(5.0f).value << std::endl;
+    std::cout << "New value of ft = (ft + 3.0f) * 1.5f / 5.0f = " << ( ft.add(3.0f).multiply(1.5f).divide(5.0f))  << std::endl;
        
     std::cout << "---------------------\n" << std::endl; 
     
     // DoubleType/IntType object instanciation and method tests
     // --------
-    std::cout << "Initial value of dt: " << dt.value << std::endl;
-    std::cout << "Initial value of it: " << it.value << std::endl;
+    std::cout << "Initial value of dt: " << dt << std::endl;
+    std::cout << "Initial value of it: " << it << std::endl;
     // --------
     std::cout << "Use of function concatenation (mixed type arguments) " << std::endl;
-    std::cout << "New value of dt = (dt * it) / 5.0f + ft = " << (dt.multiply(it).divide(5.0f).add(ft).value) << std::endl;
+    std::cout << "New value of dt = (dt * it) / 5.0f + ft = " << ( dt.multiply( static_cast<int>(it) ).divide( static_cast<double>(5.0f) ).add( static_cast<double>(ft)) ) << std::endl;
 
     std::cout << "---------------------\n" << std::endl; 
     
     // Intercept division by 0
     // --------
     std::cout << "Intercept division by 0 " << std::endl;
-    std::cout << "New value of it = it / 0 = " << it.divide(0).value << std::endl;
-    std::cout << "New value of ft = ft / 0 = " << ft.divide(0).value << std::endl;
-    std::cout << "New value of dt = dt / 0 = " << dt.divide(0).value << std::endl;
+    std::cout << "New value of it = it / 0 = " << ( it.divide(0) ) << std::endl;
+    std::cout << "New value of ft = ft / 0 = " << ( ft.divide(0) ) << std::endl;
+    std::cout << "New value of dt = dt / 0 = " << ( dt.divide(0) ) << std::endl;
 
     std::cout << "---------------------\n" << std::endl; 
 
-    FloatType ft( 5.5f );
-    DoubleType dt( 11.1 );
-    IntType it ( 34 );
-    DoubleType pi( 3.14 );
-
-    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( ft ).multiply( ft ).divide( it ) << std::endl;
-    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( 3 ).add( it ) << std::endl;
-    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide( pi ).multiply( dt ).subtract( ft ) << std::endl;
-    std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
-    std::cout << it.multiply(it).divide(0).divide(0.0f).divide(0.0) << std::endl;
-    
-    std::cout << "FloatType x IntType  =  " << it.multiply( ft ) << std::endl;
-    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( dt ).add( ft ).multiply( 24 ) << std::endl;
-
+    part3();
 
     std::cout << "good to go!\n";
-    */
 
     return 0;
 }
@@ -339,8 +348,4 @@ error: integer division by zero is an error and will crash the program!
 FloatType x IntType  =  13143546
 (IntType + DoubleType + FloatType) x 24 = 315447336
 good to go!
-
-
-
-
 */
