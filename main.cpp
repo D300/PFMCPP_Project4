60f9257004883d41742cd4d35fc7640ebb4ed6a1
D300 <mail@dirkhandreke.com>
2020-12-29 17:59:55 +0000

    added LeakObjectDetector.h and Atmoic.h

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ Create a branch named Part9
  2) move these macros after the JUCE_LEAK_DETECTOR macro :
  */
 
+
 #define JUCE_DECLARE_NON_COPYABLE(className) \
             className (const className&) = delete;\
             className& operator= (const className&) = delete;
@@ -20,6 +21,7 @@ Create a branch named Part9
 #define JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(className) \
             JUCE_DECLARE_NON_COPYABLE(className) \
             JUCE_LEAK_DETECTOR(className)
+
 
 /*
  3) add JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Temporary) to the end of the  Temporary<> struct
@@ -69,9 +71,16 @@ i cubed: 531441
 Use a service like https://www.diffchecker.com/diff to compare your output. 
 */
 
-
 #include <iostream>
 #include <typeinfo>
+
+#include <cmath>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <memory>
+
+#include "LeakedObjectDetector.h"
 
 template<typename NumericType>
 struct Temporary
@@ -81,22 +90,19 @@ struct Temporary
         std::cout << "I'm a Temporary<" << typeid(v).name() << "> object, #"
                   << counter++ << std::endl;
     }
-   
+
     operator NumericType() const { return v; } /* read-only function */
     operator NumericType&() { return v; } /* read/write function */
 private:
     static int counter;
     NumericType v;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Temporary)
 };
+
 
 template<typename NumericType>
 int Temporary<NumericType>::counter {0};
-
-#include <cmath>
-#include <iostream>
-#include <functional>
-#include <algorithm>
-#include <memory>
 
 //============================================================
 struct A {};
@@ -204,7 +210,13 @@ struct Numeric
 
 private:
     std::unique_ptr<Type> ownedType; 
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Numeric)
 };
+
+
+
+
 
 //========================================================
 // #5 FREE FUNCS
